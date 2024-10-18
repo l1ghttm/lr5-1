667754781993f6457c3b21ed21d4481d567e68a9
l1ghttm <ps4j@bk.ru>
2024-10-18 15:38:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-stroka = str(input("Напишите строку: "))
-all_glasn = "ауоиэыяюеёАУОИЭЫЯЮЕЁ"
-all_soglasn =  "бвгджзйклмнпрстфхцчшщБВГДЖЗЙКЛМНПРСТФХЦЧШЦ"
-glasn = 0
+stroka = str(input("Напишите строку: ")) #запрос на введение строки
+all_glasn = "ауоиэыяюеёАУОИЭЫЯЮЕЁ" #список всех гласных 
+all_soglasn =  "бвгджзйклмнпрстфхцчшщБВГДЖЗЙКЛМНПРСТФХЦЧШЦ" #список всех согласных
+glasn = 0 
 soglasn = 0
-for i in stroka:
-  if i in all_glasn:
-        glasn +=1
+for i in stroka: # Цикл фор
+  if i in all_glasn: 
+        glasn +=1 #определяется кол-во гласных
   elif i in all_soglasn:
-        soglasn +=1 
-print(f"длина строки {len(stroka)}")
-print(f"кол-во гласных {glasn}")
-print(f"кол-во согласных {soglasn} ")
+        soglasn +=1 #определяется кол-во согласных
+print(f"длина строки {len(stroka)}") #вывод кол-во длины строки
+print(f"кол-во гласных {glasn}")#вывод кол-во гласных строки
+print(f"кол-во согласных {soglasn} ")#вывод кол-во согласных строки
